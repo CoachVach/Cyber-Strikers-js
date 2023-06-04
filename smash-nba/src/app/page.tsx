84ed9835bbe6a0f5c9fb835986da8b0fd058d5d0
Erik Kovach <78232947+CoachVach@users.noreply.github.com>
2023-06-03 22:18:37 -0300

    cosillas

--- smash-nba/src/app/page.tsx
+++ smash-nba/src/app/page.tsx
@@ -4,6 +4,7 @@ import TeamsComponent from './Components/TeamsComponent';
 import CustomNavbar from './Components/Navbar';
 import Item from './Components/Item';
 import { useEffect, useState } from 'react';
+import './StyleComponents/Inicio.css'
 
 export default function Home() {
   const [cartItems, setCartItems] = useState<Item[]>([]);
